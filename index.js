93e9eb9f8bb6a2ddf94d939efb4ce62c850dda1f
Zitle-Nancy <ancizj@hotmail.com>
2018-01-28 02:00:10 -0600

    cambia la ejecucion de produccion a dev y por ende modifica el direccionamiento de nuestro script, poniendo el puerto en donde esta corriendo nuestro archivo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const container = document.getElementById('app');
-const texto = <h1> Hola Mundo React</h1>;
+const texto = <h1> Hola Nancy </h1>;
 ReactDOM.render( texto, app);
