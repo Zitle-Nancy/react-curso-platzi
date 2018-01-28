ab79c52a92cb91ce4becb66778749fc456e45b68
Zitle-Nancy <ancizj@hotmail.com>
2018-01-28 01:57:44 -0600

    importa react y lo instala como dependencia

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hola');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app');
+const texto = <h1> Hola Mundo React</h1>;
+ReactDOM.render( texto, app);
