22dec6e0c1d7489e4b33a6711bc62c78af86fc14
Zitle-Nancy <ancizj@hotmail.com>
2018-01-28 23:40:27 -0600

    pone el vaLor requerido

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom'; //Una forma de hacerlo
 import Media from './src/playlist/components/media';
 // para enviar numeros es {123}..numeros
 render( <Media title="Responsive Design" 
-			   
+			   author = "Nancy Zitle"
 			   type = "video"
 			   image = ".././images/covers/responsive.jpg" 
 		/> , app);
