2119dae46f499417d14adea6f709d35b78f26f05
Zitle-Nancy <ancizj@hotmail.com>
2018-01-28 21:16:18 -0600

    crea nuestro primer componente

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; //version antigua
+import { render } from 'react-dom'; //Una forma de hacerlo
+import Media from './src/playlist/components/media';
 
 const container = document.getElementById('app');
 const texto = <h1> Hola Nancy </h1>;
-ReactDOM.render( texto, app);
+
+render( <Media /> , app);
+
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<img 
+						src="" 
+						alt="Deberia de salir una imagen"
+						width={260}
+						height={160}
+					/>
+					<h3>Por que..</h3>
+					<p>Nancy</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Media;
