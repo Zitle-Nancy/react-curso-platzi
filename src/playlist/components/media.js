281cc4ea7e072291108bb3f2a5bd5c126cd868b9
Zitle-Nancy <ancizj@hotmail.com>
2018-01-30 18:43:32 -0600

    usando arrow function para evitar el bind con el metodo constructor, una manera mas facil de hacer bind en ES7

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
-	constructor(props){
-		super(props)
-		this.handleClick = this.handleClick.bind(this);
-	}
+	// constructor(props){
+	// 	super(props)
+	// 	this.handleClick = this.handleClick.bind(this);
+	// }
 	/*aqui van las funciones*/
-	handleClick(event) {
-		console.log(this.props.title);
+	handleClick = (event) => {
+		console.log(this.props.image);
 	}
 	render() {
 		return (
