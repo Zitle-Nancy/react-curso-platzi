a8774589959a893efd589a21f739d50cc01c14d8
Zitle-Nancy <ancizj@hotmail.com>
2018-02-03 20:47:55 -0600

    crea el state y lo modifica usando ECMS6, y creando una manera mas legible y dejando de usar el metodo constructor

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,26 +3,32 @@ import PropTypes from 'prop-types';
 import './media.css';
 
 class Media extends Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			author:props.author
-		}
-		// debemos bindear la funcion, dentro de nuestro metodo constructor
-		this.handleClick = this.handleClick.bind(this);
+	state = {
+		author: 'No entiendo'
 	}
+	// constructor(props){
+	// 	super(props)
+	// 	this.state = {
+	// 		author:props.author
+	// 	}
+	// 	// debemos bindear la funcion, dentro de nuestro metodo constructor
+	// 	this.handleClick = this.handleClick.bind(this);
+	// }
 	// debemos crear al funcion 
-	handleClick(event) {
-		console.log(this.props.image);
-		this.setState({
-			author:"Prueba"
-		})
-	}
+	// handleClick(event) {
+	// 	// console.log(this.props.image);
+	// 	this.setState({
+	// 		author:"Prueba"
+	// 	})
+	// }
 	/*aqui van las funciones*/
 	// ejemplo hecho en ECMS6
-	// handleClick = (event) => {
-	// 	console.log(this.props.image);
-	// }
+	handleClick = (event) => {
+		// console.log(this.props.image);
+		this.setState({
+			author:'Prueba con ECMS6'
+		})
+	}
 	render() {
 		return (
 			// handleClick, solo es el nombre de la funcion, puede ir el que sea
