189a164b308f34fb78081d1e538d586d5fceec75
Zitle-Nancy <ancizj@hotmail.com>
2018-02-27 23:31:04 -0600

    cambia un componente a componente puro y cambia otro componente a componente funcional

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media.js';
 import './playlist.css';
 
-export default class Playlist extends Component {
-	render() {
-		const categories = this.props.data.categories;
-		console.log(categories);
-		return(
-				<div className = 'playlist'>
-					{
-						categories.map((elemento,indice) => {
-							const playlists = elemento.playlist;
-							return playlists.map((playlist, item) => {
-								return <Media {...playlist} key = {item} />
-							})
-						})
-					}
-				</div>
-			);
-	}
+function Playlist(props){
+	const categories = props.data.categories;
+	// console.log(categories);
+	return(
+		<div className = 'playlist'>
+		Esta es un componente puro
+			{
+				categories.map((elemento,indice) => {
+					const playlists = elemento.playlist;
+					return playlists.map((playlist, item) => {
+						return <Media {...playlist} key = {item} />
+					})
+				})
+			}
+		</div>
+	);
 }
+
+export default Playlist;
