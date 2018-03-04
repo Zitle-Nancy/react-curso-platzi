17d3f0c89fee983987693680dd5258688e289a80
Zitle-Nancy <ancizj@hotmail.com>
2018-03-04 10:07:59 -0600

    pasamos los estilos de forma antigua

--- src/icons/components/icon.js
+++ src/icons/components/icon.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function Icon(props){
 	return(
-			<div>
+			<svg 
+				fill = {props.color}
+				height = {props.height}
+				width = {props.width}
+				viewBox = "0 0 32 32" 
+			>
 				{this.props.children}
-			</div>
+			</svg>
 		)
 } 
 
