40e99e5347145dd09b0dfea439ea09ef16f327a9
Zitle-Nancy <ancizj@hotmail.com>
2018-03-04 09:59:30 -0600

    generamos components puros y funcionales

---
+++ src/icons/components/icon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Icon(props){
+	return(
+			<div>
+				{this.props.children}
+			</div>
+		)
+} 
+
+export default Icon;
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play.js';
 
 function Playlist(props){
 	const categories = props.data.categories;
 	// console.log(categories);
 	return(
 		<div className = 'playlist'>
-		Esta es un componente puro
+			<Play />
 			{
 				categories.map((elemento,indice) => {
 					const playlists = elemento.playlist;
