cf51d48bc68633728415ebfdee803af864940307
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-03-11 02:52:38 -0600

    agrega la clase Modal a mi elemento funcional pero me genera un error si uso className y si mi clase en css es en minuscula no la acepta

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,6 +3,7 @@ import HomeLayout from '../components/home-layout';
 import Categories from '../../category/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal-container';
+import Modal from '../../widgets/components/modal';
 
 class Home extends Component {
 	render(){
@@ -11,7 +12,9 @@ class Home extends Component {
 					<Related />
 					<Categories categories = {this.props.data.categories} />
 					<ModalContainer>
-						<h1>Portales</h1>
+						<Modal>
+							<h1>Portales</h1>
+						</Modal>
 					</ModalContainer>
 				</HomeLayout>
 			)
---
+++ src/widgets/components/modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './modal.css';
+
+export default function Modal(props){
+  return(
+    <div class = "Modal">
+      Esto es un modal
+      {props.children}
+    </div>
+  )
+}
