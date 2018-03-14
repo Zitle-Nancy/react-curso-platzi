9d8a567924747b65b7de27c6bf3d9122cb3c3b06
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-03-14 00:01:29 -0600

    se genera una carpeta de errores con container y components

---
+++ src/error/components/regular-error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RegularError(){
+    return(
+        <h1 style={{color:'white'}}>Ha ocurrido un error</h1>
+    )
+}
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,11 +4,11 @@ import Categories from '../../category/components/categories';
 import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal-container';
 import Modal from '../../widgets/components/modal';
+import HandleError from '../../error/container/handle-error';
 
 class Home extends Component {
 	state = {
 		modalVisible: false,
-		handleError:false,
 	}
 	handleOpenModal = () => {
 		this.setState({
@@ -20,30 +20,23 @@ class Home extends Component {
 			modalVisible: false,
 		})
 	}
-	componentDidCatch(error, info){
-		this.setState({
-			handleError:true
-		})
-	}
-
 	render(){
-		if(this.state.handleError){
-			return <p>OHH hubo un error</p>
-		}
 		return(
-				<HomeLayout>
-					<Related />
-					<Categories categories = {this.props.data}
-					 handleOpenModal={this.handleOpenModal}
-					/>
-					{this.state.modalVisible &&
-						<ModalContainer>
-							<Modal handleClick = {this.handleCloseModalClck}>
-								<h1>Portales</h1>
-							</Modal>
-						</ModalContainer>
-					}
-				</HomeLayout>
+				<HandleError>
+					<HomeLayout>
+						<Related />
+						<Categories categories = {this.props.data}
+						handleOpenModal={this.handleOpenModal}
+						/>
+						{this.state.modalVisible &&
+							<ModalContainer>
+								<Modal handleClick = {this.handleCloseModalClck}>
+									<h1>Portales</h1>
+								</Modal>
+							</ModalContainer>
+						}
+					</HomeLayout>
+				</HandleError>
 			)
 	}
 }
