00808ff4e1649fa7de11b822c22d7bf573ca793b
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-03-26 00:31:08 -0600

    crea componentes para el video y el titulo

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
-
+import Title from '../components/title';
 export default class VideoPlayer extends Component{
     render(){
         return(
             <VideoPlayerLayout>
+							<Title
+								title="Mi primer video en React :)"
+							/>
 							<Video 
 								autoplay={true}
 								src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
