0e6edb6161d3054490f60d2efa47669e285b10ea
Nancy Zitle Juarez <nancy@MacBook-Pro-de-Nancy.local>
2018-04-01 21:14:21 -0500

    modifica el formato del tiempo transcurrido

--- src/player/components/timer.js
+++ src/player/components/timer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './timer.css';
 
+function leftPad(number){
+  const pad = '00';
+  // console.log(number.length);
+  // console.log(pad.length);
+  // console.log(pad.substring(0, pad.length - number.length) + number);
+  return pad.substring(0, pad.length - number.length) + number;
+}
+
 function formattedTimer(secs){
   const minutes = parseInt(secs / 60 , 10);
   const seconds = parseInt(secs % 60 , 10); 
-  return `${minutes} : ${seconds}`
+  return `${minutes} : ${leftPad(seconds.toString())}`
 }
-
 
 export default function Timer(props){
   return(
@@ -16,7 +23,7 @@ export default function Timer(props){
             {
             formattedTimer(props.currentTime)
             } 
-              / 
+                /  
             {
               formattedTimer(props.duration)
             }
